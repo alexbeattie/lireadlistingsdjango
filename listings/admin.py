1e98c1202813db4cbab418a7e79e2b8638f82381
Alex Beattie <artisanbranding@me.com>
2024-10-20 15:11:00 -0700

    adding to repo

--- listings/admin.py
+++ listings/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.db import models
 from django.forms import Textarea
 from django.utils.html import format_html
-
-
+from django.contrib.gis.admin import GISModelAdmin
+from django.contrib.gis import admin
 from .models import Listing, Photo
 
 class PhotoInline(admin.TabularInline):
@@ -12,7 +12,7 @@ class PhotoInline(admin.TabularInline):
     fields = ('image', 'description')
 
 @admin.register(Listing)
-class ListingAdmin(admin.ModelAdmin):
+class ListingAdmin(GISModelAdmin):
     inlines = [PhotoInline]
     list_display = ('title', 'price', 'property_type', 'bedrooms', 'bathrooms', 'is_published', 'list_date', 'realtor', 'photo_count', 'main_image_preview')
     list_filter = ('is_published', 'realtor', 'property_type', 'island', 'view_type')
@@ -26,7 +26,7 @@ class ListingAdmin(admin.ModelAdmin):
             'fields': ('property_type', 'bedrooms', 'bathrooms')
         }),
         ('Location', {
-            'fields': ('city', 'state', 'island', 'view_type')
+            'fields': ('city', 'state', 'zipcode', 'island', 'view_type', 'location')
         }),
         ('Images | Media', {
             'fields': ('main_image', 'main_image_preview', 'floor_plan_one', 'floor_plan_two', 'video_or_3d_tour')
@@ -35,6 +35,16 @@ class ListingAdmin(admin.ModelAdmin):
             'fields': ('is_published', 'list_date')
         }),
     )
+    # GIS-specific options
+    map_template = 'gis/admin/openlayers-osm.html'
+
+    gis_widget_kwargs = {
+        'attrs': {
+            'default_lat': 48.816662,  # Default latitude (e.g., for Victoria, BC)
+            'default_lon': -123.508873,  # Default longitude
+            'default_zoom': 12,
+        },
+    }
     formfield_overrides = {
         models.TextField: {'widget': Textarea(attrs={'rows': 10, 'cols': 40})},
     }
